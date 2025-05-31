09a40c6245f58ca9deae42120dfd5da4920bd454
Kode <Olorik362@gmail.com>
2025-05-31 17:19:41 +0100

    Switched to using css query selectors for selecting dom elemnts

--- chessboard.js
+++ chessboard.js
@@ -1,4 +1,4 @@
-const board = document.getElementById('board');
+const board = document.getElementByQuerySelector('table.board');
 const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const reversedAlphabets = [...alphabets].reverse();
 
